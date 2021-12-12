6261564eb42caf1f3e985796acb9de072769cc77
fengsonglin@happy-seed.com <fengsonglin@fengsonglins-MacBook-Pro.local>
2021-12-13 00:54:47 +0800

    android 运行成功 & AwesomeProject2/

--- FasterListDemo/index.js
+++ FasterListDemo/index.js
@@ -4,6 +4,11 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import { StackNavigator } from 'react-navigation';
+
+import FlatListDemo from "./pages/FlatListDemo";
+
 import {name as appName} from './app.json';
+
 
 AppRegistry.registerComponent(appName, () => App);
