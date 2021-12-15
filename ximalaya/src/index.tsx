f05135b0b2125e03c71bee8f9fbaa5cbee061664
fengsonglin@happy-seed.com <fengsonglin@fengsonglins-MacBook-Pro.local>
2021-12-15 23:50:59 +0800

    喜马拉雅项目初见

---
+++ ximalaya/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {test} from './pages/test/test/index';
+
+test();
+
+class App extends React.Component<any, any> {
+  render() {
+    return (
+      <View>
+        <Text>测试内容林德洛夫龙卷风d App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ ximalaya/src/pages/test/test/index.ts
@@ -0,0 +1,4 @@
+// 这里如果报错，需要配置tsconfig文件里面的baseurl和path
+import {test} from '@/utils/index';
+
+export {test};
